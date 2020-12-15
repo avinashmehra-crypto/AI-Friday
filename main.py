65858bf6427b0b19b4d015bec2326aa62d8d3e0a
avinashmehra-crypto <73026188+avinashmehra-crypto@users.noreply.github.com>
2020-12-16 00:47:30 +0530

    friday.py

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ def take_command():
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
+            if 'friday' in command:
+                command = command.replace('friday', '')
                 print(command)
     except:
         pass
     return command
 
 
-def run_alexa():
+def run_friday():
     command = take_command()
     print(command)
     if 'play' in command:
@@ -41,15 +41,15 @@ def run_alexa():
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
+    elif 'who is' in command:
+        person = command.replace('who  is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
     elif 'date' in command:
         talk('sorry, I have a headache')
     elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
+        talk('I am in a relationship with your data')
     elif 'joke' in command:
         talk(pyjokes.get_joke())
     else:
@@ -57,4 +57,4 @@ def run_alexa():
 
 
 while True:
-    run_alexa()
+    run_friday()
